654f94c955dc9b0fb12d0f3c2319b5df9af1d562
Beniamin Savu <benisavu@gmail.com>
2017-04-18 18:42:20 +0300

    Created project structure. Made the project configuration to run a basic Spring Hello World. Configured the Database connection

---
+++ Application/web/src/main/java/org/hacktronic/controller/Test.java
@@ -0,0 +1,23 @@
+package org.hacktronic.controller;
+
+public class Test {
+	private int value;
+	private int sum;
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+
+}
